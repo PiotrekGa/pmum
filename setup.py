6bc36bf75d50fba4a6a59aea56705a2ecf2f0e16
PiotrekGa <piotrek.gabrys@gmail.com>
2021-01-09 14:22:14 +0100

    pipfile added

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
+        'numpy>=1.19.5',
         'pandas>=1.2.0',
-        'numpy>=1.19.5'
         'scikit-learn>=0.24.0',
         'matplotlib>=3.3.3',
         'seaborn>=0.11.1'],
